9f0583095350ffd04ec64efa9a826698b7eb869b
joowdx <joowdx@pm.me>
2022-12-13 13:55:00 +0800

    first commit

---
+++ app/commons.py
@@ -0,0 +1,3 @@
+STATIC_ROOT = os.path.normpath(join(os.path.dirname(BASE_DIR), 'static'))
+STATICFILES_DIRS = []
+STATIC_URL = '/static/'
---
+++ app/views.py
@@ -0,0 +1,17 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from zk import ZK, const
+
+# Create your views here.
+@api_view(['GET'])
+def get(request):
+    conn = None
+    zk = ZK(ip = request.query_params.get('ip'), port = int(request.query_params.get('port') or 0) or 4370, timeout = 3)
+    try:
+        conn = zk.connect()
+        conn.disable_device()
+        attlogs = map(lambda x: {'uid': x.user_id, 'date': x.timestamp, 'state': x.punch}, conn.get_attendance())
+    finally:
+        if conn:
+            conn.disconnect()
+    return Response(attlogs)
